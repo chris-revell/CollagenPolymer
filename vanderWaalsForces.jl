94204b6f200948c0e63ad11bf3ab954505978776
chris-revell <christopher.revell@crick.ac.uk>
2020-05-04 21:18:56 +0100

    Restrict van der waals attraction to achieve stepping

--- vanderWaalsForces.jl
+++ vanderWaalsForces.jl
@@ -15,14 +15,27 @@ using .LennardJones
 
 @inline function vanderWaalsForces!(pos::MMatrix,F::MMatrix,Nmonomers::Int64,Ndomains::Int64,ϵ::Float64,σ::Float64,D::MArray{Tuple{3},Float64,1,3})
 
+    # Loop over all particles
     @inbounds for ii=1:Ndomains*Nmonomers
+        # For each particle, loop over all other particles
         @inbounds for jj=1:Ndomains*Nmonomers
-            if floor(Int8,(ii-1)/Ndomains)!=floor(Int8,(jj-1)/Ndomains)
-                D = pos[jj,:] .- pos[ii,:]
+            # Exclude particles in the same monomer
+        #    if floor(Int8,(ii-1)/Ndomains)!=floor(Int8,(jj-1)/Ndomains)
+        #
+        #        mod(ii-1,Ndomains)+1) $(mod(jj-1,Ndomains)+1)
+            D = pos[jj,:] .- pos[ii,:]
+            Dmag_sq = dot(D,D)
+            if (ii-1)%Ndomains+1 == (jj-1)%Ndomains+4
                 lennardJones!(D,ϵ,σ)
                 F[ii,:] .+= D
                 F[jj,:] .-= D
+            elseif Dmag_sq < σ^2
+                lennardJones!(D,ϵ,σ)
+                F[ii,:] .+= D
+                F[jj,:] .-= D
+            else
             end
+        
         end
     end
 end
