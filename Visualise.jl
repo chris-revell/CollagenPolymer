61f966f8d479b3122e35f6624803d89fbc621363
chris-revell <christopher.revell@crick.ac.uk>
2020-11-24 12:58:47 +0000

    Tidying

--- Visualise.jl
+++ Visualise.jl
@@ -18,13 +18,13 @@ using Printf
 
 
 function visualise(foldername,nParticles,σ)
-    nImages = 100
     data = readdlm(foldername*"/output.txt",',',Float64)
+    nImages = floor(Int64,size(data)[1]/nParticles)
     r = fill(Point3(0.0,0.0,0.0),nParticles)
     set_theme!(show_axis = false, scale_plot = false, resolution = (800, 800))
-    for i in 0:nImages
+    for i in 0:nImages-1
         scene = Scene()
-        meshscatter!(data[i*nParticles+1:(i+1)*nParticles,:],markersize=σ,color=:green)
+        meshscatter!(data[i*nParticles+1:(i+1)*nParticles,:],markersize=σ/2.0,color=:green)
         #r .= Point3.(eachslice(data[i*nParticles+1:(i+1)*nParticles,:],dims=1))
         #for j=1:nParticles
         #    mesh!(Sphere(r[j],σ),color=:green)
